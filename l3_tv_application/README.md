63a3ba4a3b675e4de2625b89cebb46a9593283f8
ortub10 <ortub10@gmail.com>
2022-12-19 19:49:32 +0200

    part 3 - use json

--- l3_tv_application/README.md
+++ l3_tv_application/README.md
@@ -9,3 +9,9 @@
 ![part2](https://user-images.githubusercontent.com/102150516/208476524-bb98cb7f-3457-44eb-afaf-16715c7cc576.png)
 
 ---
+
+### part 3:
+
+![part3](https://user-images.githubusercontent.com/102150516/208488189-72036b0b-25a5-45ac-b9f6-62aaa966919a.png)
+
+---
--- l3_tv_application/js/app_tv.js
+++ l3_tv_application/js/app_tv.js
@@ -3,21 +3,31 @@ window.onload = function () {
 };
 
 const crateAllMovies = function () {
-  let movie1 = new TvClass(
-    "The simpsons",
-    "simpsons.jpg",
-    "Series",
-    10000,
-    "simpsons info....."
-  );
-  movie1.render();
-
-  let movie2 = new TvClass(
-    "Narcos",
-    "narcos.jpg",
-    "Series",
-    20000,
-    "narcos info....."
-  );
-  movie2.render();
+  json_ar.forEach(function (item) {
+    // Creating movies from file json(but not real json, It just comes to simulate the same behavior)
+    let movie = new TvClass(
+      item.name,
+      item.image,
+      item.kind,
+      item.views,
+      item.descrption
+    );
+    movie.render();
+  });
+  // let movie1 = new TvClass(
+  //   "The simpsons",
+  //   "simpsons.jpg",
+  //   "Series",
+  //   10000,
+  //   "simpsons info....."
+  // );
+  // movie1.render();
+  // let movie2 = new TvClass(
+  //   "Narcos",
+  //   "narcos.jpg",
+  //   "Series",
+  //   20000,
+  //   "narcos info....."
+  // );
+  // movie2.render();
 };
